b7975b77c7b175de61f64869b939af61ff1251ea
Soplix <kacper0infinity@gmail.com>
2018-11-24 09:25:17 +0100

    Second Commit
    + snake tail
    + apple has other color than snake
    + script was rebuilded
    +

--- Snake000/Snake3/script.js
+++ Snake000/Snake3/script.js
@@ -14,11 +14,17 @@ let snakeY = 0;
 let snakeWidth = 40;
 let snakeHeight = 40;
 
-let appleAmount = 0;
-let isApplesCaught = 0;
+let snakeLength = 3;
 
-let appleX = (Math.floor((Math.random() * 20) + 1)) * 40;
-let appleY = (Math.floor((Math.random() * 20) + 1)) * 40;
+let appleAmount = 0;
+
+let tail = [];
+
+let applePos = {
+    x: (Math.floor((Math.random() * 20) + 1)) * 40, 
+    y: (Math.floor((Math.random() * 20) + 1)) * 40
+};
+
 
 // let snake = {
 //     x: 0,
@@ -36,7 +42,7 @@ let height = 800;
 let coordinatesX;
 let coordinatesY;
 
-function checkCollision(x, y, w, h){
+function checkWorldCollision(x, y, w, h){
     if (x + w > width) {
         snakeX = 0;
     }
@@ -51,36 +57,52 @@ function checkCollision(x, y, w, h){
     }
 }
 
-function checkSecondObject(){
-    if (appleX === snakeX && appleY === snakeY) {
-        appleAmount = appleAmount + 1
-
-        amountOfApple.textContent = appleAmount;
-        
-        appleX = (Math.floor((Math.random() * 19) + 1)) * 40;
-        appleY = (Math.floor((Math.random() * 19) + 1)) * 40;
-
-
-        context.clearRect(0, 0, width, height);
-
-        context.fillRect(snakeX, snakeY, snakeWidth, snakeHeight);
-
-        isApplesCaught = 0;
-
-
+function updateSnakeTail() {
+    tail.push({x: snakeX, y: snakeY});
+    if (tail.length > snakeLength)
+    {
+        tail.shift();
     }
+}
+
+function checkCollisionAndRollPosition (objA, objB) {
+    if (objA.x === objB.x && objA.y === objB.y) {
+        objA.x = (Math.floor((Math.random() * 19) + 1)) * 40;
+        objA.y = (Math.floor((Math.random() * 19) + 1)) * 40;
+        return true;
+    }
+    return false;
 }
 
 function gameLoop(){
     // update 
     snakeX += directionX * speed;
     snakeY += directionY * speed;
-    checkCollision(snakeX, snakeY, snakeWidth, snakeHeight);
+    let snakePos = {
+        x: snakeX,
+        y: snakeY
+    };
+    updateSnakeTail();
+
+    checkWorldCollision(snakeX, snakeY, snakeWidth, snakeHeight);
+    if (checkCollisionAndRollPosition(applePos, snakePos)){
+        appleAmount++;
+        snakeLength++;
+    }
+    updateUI();
+
 
     // render / draw
     context.clearRect(0, 0, width, height);
-    context.fillRect(snakeX, snakeY, snakeWidth, snakeHeight);
+    context.fillStyle="#FF0000";
+    context.fillRect(applePos.x, applePos.y, snakeWidth, snakeHeight);
 
+    tail.forEach(function (tailPos){
+        context.fillStyle="#009900";    
+        context.fillRect(tailPos.x, tailPos.y, snakeWidth, snakeHeight);
+    });
+    context.fillStyle="#00FF00";    
+    context.fillRect(snakeX, snakeY, snakeWidth, snakeHeight);
 
 }
 
@@ -108,28 +130,12 @@ function initHandleInput(){
     );
 }
 
-function xID() {
-    poleXID.textContent = (snakeX+snakeWidth)/40;
-}
-
-function yID() {
+function updateUI() {
     poleYID.textContent = (snakeY+snakeHeight)/40;
+    poleXID.textContent = (snakeX+snakeWidth)/40;
+    amountOfApple.textContent = appleAmount;
 }
 
 initHandleInput();
 
 setInterval(gameLoop, 200);
-
-setInterval(xID, 200);
-setInterval(yID, 200);
-
-function appleAdd () {
-    if (isApplesCaught === 0) {
-        context.fillRect(appleX, appleY, snakeWidth, snakeHeight);
-    }
-}
-setInterval(appleAdd, 200)
-
-setInterval(checkSecondObject, 200);
-
-
